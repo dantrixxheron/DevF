358b37680e570cd086d43e1b747e8745962ccc5e
dantrixxheron <danielacmj.04@gmail.com>
2025-02-04 20:53:13 -0600

    add module3 class1

---
+++ modulo3/Javascript-Desde-Cero/tipo-de-dato.js
@@ -0,0 +1,44 @@
+//este es un comentario en javascript
+//console.log es una funcion que se utiliza para imprimir en consola
+console.log("Hola mundo"); 
+
+//sintaxis es el orden en que se escriben las cosas y semántica es el significado de las cosas
+// declaracion de variables
+var nombre = "Juan";  //string
+var edad = 30; //number
+var sueldo = 1.234; //number
+var casado = false; //boolean
+var vacunas = true; //boolean
+var hijos = null; //null
+var dato = undefined; //undefined
+//si ponemos null y buscamos el typeof nos saldra object esto se debe a un error de javascript que no se ha corregido por temas de compatibilidad
+
+console.log(typeof(nombre)); //string
+console.log(typeof(edad)); //number
+console.log(typeof(sueldo)); //number
+console.log(typeof(casado)); //boolean
+console.log(typeof(vacunas)); //boolean
+console.log(typeof(hijos)); //object
+console.log(typeof(dato)); //undefined
+//ejemplo de semantica
+/*var nombre = "Francia";
+var pais = 20; está bien escrito pero no tiene sentido */ 
+
+
+// Ejercicios de Campus
+console.log("Ejercicios de Campus");
+console.log(typeof(42));
+console.log(typeof("Veinticinco"));
+console.log(typeof(-666));
+console.log(typeof(true));
+console.log(typeof(0));
+console.log(typeof(''));
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(typeof(false));
+console.log("Añadidos");
+console.log(typeof(NaN));
+console.log(typeof(0.0));
+console.log(typeof([]));
+console.log(typeof({}));
+console.log(typeof(3/5));
