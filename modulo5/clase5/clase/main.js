0b74d8e4d1d13830c2f2cca7e6eedde813a4dd89
dantrixxheron <danielacmj.04@gmail.com>
2025-05-15 20:18:24 -0600

    add clase 5 files

---
+++ modulo5/clase5/clase/main.js
@@ -0,0 +1,44 @@
+const registerButton = document.querySelector('#register-button')
+const registerForm =document.querySelector('.form-container form')
+const togglePasswordButtons=[...document.querySelectorAll('.toggle-password')]
+
+const handleRegister = (e) => {
+    // cancelar el compartamiento por defecto del formulario (es decir, que no se recargue la página)
+    e.preventDefault()
+    const form = e.target
+    const userEmail=form.children[1].value;
+    if(!userEmail){
+        alert('El email es obligatorio')
+        return;
+    }
+    const userPassword=form.children[3].value;
+    const userPasswordConfirm=form.children[5].value;
+    if(userPassword !== userPasswordConfirm){
+        alert('Las contraseñas deben coincidir')
+        return;
+    }
+    const regexPassword = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+    if(regexPassword.test(userPasswordConfirm)){
+        alert('La contraseña debe tener al menos 8 caracteres, una letra mayuscula, minúscula y un número')
+        return;
+    }
+    console.log(userEmail, userPassword, userPasswordConfirm);
+    
+    
+}
+registerButton.addEventListener('submit',(e)=> handleRegister(e))
+
+togglePasswordButtons.forEach((button) => {
+    button.addEventListener('click', (e) => handleTogglePassword(e))
+})
+
+const handleTogglePassword = (e) => {
+    e.preventDefault()
+    const input = e.target.parentElement.children[0]
+    if(input.type === 'password'){
+        input.type = 'text'
+    }
+    else{
+        input.type = 'password'
+    }
+}
